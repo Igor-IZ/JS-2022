2d671e876834aa208874511acaaf87d3c5f67ee2
IGOR <igor.z.workshop@gmail.com>
2022-06-21 12:39:12 +0300

    Урок №20 - Условия

--- Task_0/script.js
+++ Task_0/script.js
@@ -40,3 +40,14 @@
 // personalMovieDB.movies[c] = d;
 
 // console.log(personalMovieDB);
+
+"use strict";
+
+if (4 == 9) {
+    console.log('OK!');
+} else {
+    console.log('error');
+} 
+
+    
+
